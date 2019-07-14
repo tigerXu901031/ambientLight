152013aef4f8dded7dfd09d6418da17d1a001d4a
tiger.xu <suiju1943@live.cn>
2019-07-14 10:49:44 +0800

    implement pc client that control the led mode switch via CAN

---
+++ pcClient/ledControl_client/FormMain.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace ledControl_client
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBoxLedModeSelection = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.connectToDeviceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.connectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            this.pageSetupDialog1 = new System.Windows.Forms.PageSetupDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.form1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBoxLedModeSelection);
+            this.groupBox1.Controls.Add(this.button1);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Tag = "";
+            // 
+            // comboBoxLedModeSelection
+            // 
+            this.comboBoxLedModeSelection.FormattingEnabled = true;
+            this.comboBoxLedModeSelection.Items.AddRange(new object[] {
+            resources.GetString("comboBoxLedModeSelection.Items"),
+            resources.GetString("comboBoxLedModeSelection.Items1"),
+            resources.GetString("comboBoxLedModeSelection.Items2"),
+            resources.GetString("comboBoxLedModeSelection.Items3"),
+            resources.GetString("comboBoxLedModeSelection.Items4"),
+            resources.GetString("comboBoxLedModeSelection.Items5"),
+            resources.GetString("comboBoxLedModeSelection.Items6"),
+            resources.GetString("comboBoxLedModeSelection.Items7"),
+            resources.GetString("comboBoxLedModeSelection.Items8"),
+            resources.GetString("comboBoxLedModeSelection.Items9"),
+            resources.GetString("comboBoxLedModeSelection.Items10"),
+            resources.GetString("comboBoxLedModeSelection.Items11"),
+            resources.GetString("comboBoxLedModeSelection.Items12"),
+            resources.GetString("comboBoxLedModeSelection.Items13")});
+            resources.ApplyResources(this.comboBoxLedModeSelection, "comboBoxLedModeSelection");
+            this.comboBoxLedModeSelection.Name = "comboBoxLedModeSelection";
+            this.comboBoxLedModeSelection.Tag = "1";
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connectToDeviceToolStripMenuItem});
+            resources.ApplyResources(this.menuStrip1, "menuStrip1");
+            this.menuStrip1.Name = "menuStrip1";
+            // 
+            // connectToDeviceToolStripMenuItem
+            // 
+            this.connectToDeviceToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connectToolStripMenuItem});
+            resources.ApplyResources(this.connectToDeviceToolStripMenuItem, "connectToDeviceToolStripMenuItem");
+            this.connectToDeviceToolStripMenuItem.Name = "connectToDeviceToolStripMenuItem";
+            // 
+            // connectToolStripMenuItem
+            // 
+            this.connectToolStripMenuItem.Name = "connectToolStripMenuItem";
+            resources.ApplyResources(this.connectToolStripMenuItem, "connectToolStripMenuItem");
+            this.connectToolStripMenuItem.Click += new System.EventHandler(this.ConnectToolStripMenuItem_Click);
+            // 
+            // eventLog1
+            // 
+            this.eventLog1.Log = "Application";
+            this.eventLog1.SynchronizingObject = this;
+            // 
+            // richTextBox1
+            // 
+            resources.ApplyResources(this.richTextBox1, "richTextBox1");
+            this.richTextBox1.Name = "richTextBox1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            // 
+            // FormMain
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.groupBox2);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormMain";
+            this.groupBox1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Diagnostics.EventLog eventLog1;
+        private System.Windows.Forms.ComboBox comboBoxLedModeSelection;
+        private System.Windows.Forms.ToolStripMenuItem connectToDeviceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem connectToolStripMenuItem;
+        private System.Windows.Forms.PageSetupDialog pageSetupDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.BindingSource form1BindingSource;
+    }
+}
+
---
+++ pcClient/ledControl_client/FormMain.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+/*------------兼容ZLG的数据类型---------------------------------*/
+
+//1.ZLGCAN系列接口卡信息的数据类型。
+public struct VCI_BOARD_INFO
+{
+    public UInt16 hw_Version;
+    public UInt16 fw_Version;
+    public UInt16 dr_Version;
+    public UInt16 in_Version;
+    public UInt16 irq_Num;
+    public byte can_Num;
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 20)] public byte[] str_Serial_Num;
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 40)]
+    public byte[] str_hw_Type;
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+    public byte[] Reserved;
+}
+
+/////////////////////////////////////////////////////
+//2.定义CAN信息帧的数据类型。
+unsafe public struct VCI_CAN_OBJ  //使用不安全代码
+{
+    public uint ID;
+    public uint TimeStamp;        //时间标识
+    public byte TimeFlag;         //是否使用时间标识
+    public byte SendType;         //发送标志。保留，未用
+    public byte RemoteFlag;       //是否是远程帧
+    public byte ExternFlag;       //是否是扩展帧
+    public byte DataLen;          //数据长度
+    public fixed byte Data[8];    //数据
+    public fixed byte Reserved[3];//保留位
+
+}
+
+//3.定义初始化CAN的数据类型
+public struct VCI_INIT_CONFIG
+{
+    public UInt32 AccCode;
+    public UInt32 AccMask;
+    public UInt32 Reserved;
+    public byte Filter;   //0或1接收所有帧。2标准帧滤波，3是扩展帧滤波。
+    public byte Timing0;  //波特率参数，具体配置，请查看二次开发库函数说明书。
+    public byte Timing1;
+    public byte Mode;     //模式，0表示正常模式，1表示只听模式,2自测模式
+}
+
+/*------------其他数据结构描述---------------------------------*/
+//4.USB-CAN总线适配器板卡信息的数据类型1，该类型为VCI_FindUsbDevice函数的返回参数。
+public struct VCI_BOARD_INFO1
+{
+    public UInt16 hw_Version;
+    public UInt16 fw_Version;
+    public UInt16 dr_Version;
+    public UInt16 in_Version;
+    public UInt16 irq_Num;
+    public byte can_Num;
+    public byte Reserved;
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)] public byte[] str_Serial_Num;
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)]
+    public byte[] str_hw_Type;
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)]
+    public byte[] str_Usb_Serial;
+}
+
+/*------------数据结构描述完成---------------------------------*/
+
+public struct CHGDESIPANDPORT
+{
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 10)]
+    public byte[] szpwd;
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 20)]
+    public byte[] szdesip;
+    public Int32 desport;
+
+    public void Init()
+    {
+        szpwd = new byte[10];
+        szdesip = new byte[20];
+    }
+}
+
+namespace ledControl_client
+{
+    public partial class FormMain : Form
+    {
+        byte ledMode = 0;
+
+        UInt32 devType = 4;     // USBCAN2
+        UInt32 devId = 0;
+        UInt32 canChn = 0;
+
+        UInt32 isDevOpen = 0;
+
+        /*------------兼容ZLG的函数描述---------------------------------*/
+        [DllImport("controlcan.dll")]
+        static extern UInt32 VCI_OpenDevice(UInt32 DeviceType, UInt32 DeviceInd, UInt32 Reserved);
+        [DllImport("controlcan.dll")]
+        static extern UInt32 VCI_CloseDevice(UInt32 DeviceType, UInt32 DeviceInd);
+        [DllImport("controlcan.dll")]
+        static extern UInt32 VCI_InitCAN(UInt32 DeviceType, UInt32 DeviceInd, UInt32 CANInd, ref VCI_INIT_CONFIG pInitConfig);
+        [DllImport("controlcan.dll")]
+        static extern UInt32 VCI_ReadBoardInfo(UInt32 DeviceType, UInt32 DeviceInd, ref VCI_BOARD_INFO pInfo);
+        [DllImport("controlcan.dll")]
+        static extern UInt32 VCI_GetReceiveNum(UInt32 DeviceType, UInt32 DeviceInd, UInt32 CANInd);
+        [DllImport("controlcan.dll")]
+        static extern UInt32 VCI_ClearBuffer(UInt32 DeviceType, UInt32 DeviceInd, UInt32 CANInd);
+        [DllImport("controlcan.dll")]
+        static extern UInt32 VCI_StartCAN(UInt32 DeviceType, UInt32 DeviceInd, UInt32 CANInd);
+        [DllImport("controlcan.dll")]
+        static extern UInt32 VCI_ResetCAN(UInt32 DeviceType, UInt32 DeviceInd, UInt32 CANInd);
+        [DllImport("controlcan.dll")]
+        static extern UInt32 VCI_Transmit(UInt32 DeviceType, UInt32 DeviceInd, UInt32 CANInd, ref VCI_CAN_OBJ pSend, UInt32 Len);
+        [DllImport("controlcan.dll")]
+        static extern UInt32 VCI_Receive(UInt32 DeviceType, UInt32 DeviceInd, UInt32 CANInd, ref VCI_CAN_OBJ pReceive, UInt32 Len, Int32 WaitTime);
+
+        /*------------其他函数描述---------------------------------*/
+
+        [DllImport("controlcan.dll")]
+        static extern UInt32 VCI_ConnectDevice(UInt32 DevType, UInt32 DevIndex);
+        [DllImport("controlcan.dll")]
+        static extern UInt32 VCI_UsbDeviceReset(UInt32 DevType, UInt32 DevIndex, UInt32 Reserved);
+        [DllImport("controlcan.dll")]
+        static extern UInt32 VCI_FindUsbDevice(ref VCI_BOARD_INFO1 pInfo);
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            comboBoxLedModeSelection.SelectedIndex = 0;
+            richTextBox1.Text = "No CAN connection ...\n";
+        }
+
+        private void FormMain_Close()
+        {
+            if(isDevOpen == 1)
+            {
+                VCI_CloseDevice(devType, devId);
+            }
+            else
+            {
+
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            uint sendReturnSts = 0;
+
+            VCI_CAN_OBJ sendMsgObj = new VCI_CAN_OBJ();
+            sendMsgObj.ID = 0x505;
+            sendMsgObj.RemoteFlag = 0;
+            sendMsgObj.ExternFlag = 0;
+            sendMsgObj.SendType = 0;
+            sendMsgObj.DataLen = 8;
+
+            ledMode = (byte)comboBoxLedModeSelection.SelectedIndex;
+            unsafe
+            {
+                sendMsgObj.Data[0] = (byte)ledMode;
+                sendMsgObj.Data[1] = 0;
+                sendMsgObj.Data[2] = 0;
+                sendMsgObj.Data[3] = 0;
+                sendMsgObj.Data[4] = 0;
+                sendMsgObj.Data[5] = 0;
+                sendMsgObj.Data[6] = 0;
+                sendMsgObj.Data[7] = 0;
+            }
+
+            sendReturnSts = VCI_Transmit(devType, devId, canChn, ref sendMsgObj, 1);
+            if (sendReturnSts != 0)
+            {
+                richTextBox1.AppendText("Current led mode:" + ledMode.ToString() + "\n");
+            }
+        }
+
+        private void ConnectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            uint openDevSts = 0;
+            if(isDevOpen == 0)
+            {
+                richTextBox1.AppendText("Start build CAN connection with CAN card ...\n");
+                openDevSts = VCI_OpenDevice(devType, devId, 0);
+                if (openDevSts != 1)
+                {
+                    richTextBox1.AppendText("Failed to connect the device\n");
+                    richTextBox1.AppendText("Open return code:" + openDevSts.ToString() + '\n');
+                    return;
+                }
+                else
+                {
+                    
+                }
+                isDevOpen = 1;
+
+                VCI_INIT_CONFIG canConfig = new VCI_INIT_CONFIG();
+                canConfig.Mode = 0;
+                canConfig.Timing0 = 0x00;
+                canConfig.Timing1 = 0x1C;
+
+                openDevSts = VCI_InitCAN(devType, devId, canChn, ref canConfig);
+                if(openDevSts != 1)
+                {
+                    richTextBox1.AppendText("Failed to initialize the CAN channel\n");
+                    return;
+                }
+                else
+                {
+                    richTextBox1.AppendText("Connect Succeed\n");
+                }
+
+                openDevSts = VCI_StartCAN(devType, devId, canChn);
+                if (openDevSts != 1)
+                {
+                    richTextBox1.AppendText("Failed to start the CAN channel\n");
+                    return;
+                }
+                else
+                {
+                    richTextBox1.AppendText("start Succeed\n");
+                }
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
